8271a02a2ddb863096c205e538a484f23d6ab6d2
w1ll0w <58903678+Yung71nKzH3@users.noreply.github.com>
2024-10-18 00:16:55 +0100

    Total Content Update

--- WebsiteProject/script.js
+++ WebsiteProject/script.js
@@ -6,21 +6,6 @@ scrollToTopBtn.addEventListener("click", () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
-/*Form*/
-
-const contactButton = document.getElementById('contactButton');
-const contactForm = document.getElementById('contactForm');
-
-contactButton.addEventListener('click', () => {
-  contactForm.style.display = 'block';
-});
-
-const closeButton = document.querySelector('.close');
-
-closeButton.addEventListener('click', () => {
-  contactForm.style.display = 'none';
-});
-
 /*Blog*/
 
 let slideIndex = 1;
